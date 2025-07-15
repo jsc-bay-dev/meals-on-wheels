bdc45d87c3d153b31f02e7be296212ee2ca5092c
Justin Choi <j@Justins-MBP.lan>
2025-07-15 14:35:00 -0700

    assets, layout, index, globals

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,28 @@
-import { Text, View } from "react-native";
+import { offers } from "@/constants";
+import { Text } from "@react-navigation/elements";
+import { FlatList, Pressable, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
  
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data = {offers}
+        renderItem={({item, index}) => {
+          return (
+            <View>
+              <Pressable className="bg-amber-600">
+                <Text>
+                  {item.title}
+                </Text>
+              </Pressable>
+            </View>
+          )
+        }}
+      />
+
+      
+    </SafeAreaView>
   );
 }
