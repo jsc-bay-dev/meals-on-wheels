132b05f7a7558246d499ccb18492fcd12329953b
Justin Choi <j@Justins-MBP.lan>
2025-07-15 15:56:04 -0700

    offers, SafeAreaView, FlatLists, Pressable stylng, pressed styling, Fragments

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,64 @@
-import { offers } from "@/constants";
-import { Text } from "@react-navigation/elements";
-import { FlatList, Pressable, View } from "react-native";
+import { images, offers } from "@/constants";
+import cn from 'clsx';
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
- 
+
+
 export default function Index() {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-between flex-row w-full my-5 px-5">
+        <View className="flex-start">
+          <Text className="small-bold text-primary">DELIVER TO</Text>
+          <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+            <Text className="paragraph-bold text-dark-100">Juno, AL</Text>
+            <Image source={images.arrowDown} className="size-3" resizeMode="contain"/>
+          </TouchableOpacity>
+        </View>
+        <Text>Cart</Text>
+      </View>
+
       <FlatList
-        data = {offers}
-        renderItem={({item, index}) => {
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven: boolean = index % 2 === 0;
+
           return (
             <View>
-              <Pressable className="bg-amber-600">
-                <Text>
-                  {item.title}
-                </Text>
+              <Pressable 
+                className={cn("offer-card", isEven ? "flex-row-reverse" : "flex-row")} 
+                style={{ backgroundColor: item.color }}
+                android_ripple={{color: "#fffff22"}}
+                >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className={"h-full w-1/2"}>
+                      <Image source={item.image} className="size-full" resizeMode="contain" />
+                    </View>
+
+                    <View className={cn("offer-card__info", isEven ? "pl-10" : "pr-10")}>
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor="#ffffff"
+                      />
+                    </View>
+                  </Fragment>
+                )}
               </Pressable>
             </View>
           )
         }}
+        contentContainerClassName="pb-28 px-5"
       />
 
-      
+
     </SafeAreaView>
   );
 }
