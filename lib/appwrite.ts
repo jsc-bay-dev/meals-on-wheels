6c54ef75ec76789726d70595399d6aac72ddeb8a
Justin Choi <j@Justins-MBP.lan>
2025-07-16 12:22:53 -0700

    addwrite.ts

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,53 @@
+import { CreateUserParams, SignInParams } from "@/type";
+import { Account, Avatars, Client, Databases, ID } from "react-native-appwrite";
+
 export const appwriteConfig = {
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
     platform: "com.jsc.mealsonwheels",
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
     databaseId: '6876f587001dc956d494',
     userCollectionId: '6876ff5b0032570fd232'
+}
+
+export const client = new Client();
+
+client
+    .setEndpoint(appwriteConfig.endpoint)
+    .setProject(appwriteConfig.projectId)
+    .setPlatform(appwriteConfig.platform)
+
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const avatars = new Avatars(client);
+
+export const createUser = async ({ email, password, name }: CreateUserParams) => {
+    try {
+        const newAccount = await account.create(ID.unique(), email, password, name)
+        if (!newAccount) throw Error;
+
+        await SignIn({ email, password });
+
+        const avatarUrl = avatars.getInitialsURL(name);
+        const newUser = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            {
+                email, name, accountId: newAccount.$id, avatar: avatarUrl
+            }
+
+        )
+        return newUser
+    } catch (error) {
+        throw new Error(error as string)
+    }
+}
+
+export const SignIn = async ({ email, password }: SignInParams) => {
+    try {
+        const session = await account.createEmailPasswordSession(email, password);
+    } catch (error) {
+        throw new Error(error as string)
+    }
 }
