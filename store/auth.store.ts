d645eeebff1902c01a5191cb7cb8308f7a5d0a49
Justin Choi <j@Justins-MBP.lan>
2025-07-16 23:21:40 -0700

    store

--- store/auth.store.ts
+++ store/auth.store.ts
@@ -1,3 +1,4 @@
+import { getCurrentUser } from '@/lib/appwrite';
 import { User } from '@/type';
 import { create } from 'zustand';
 
@@ -13,9 +14,29 @@ type AuthState = {
     fetchAuthenticatedUser: () => Promise<void>
 }
 
-const useStore = create((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-  updateBears: (newBears) => set({ bears: newBears }),
+const useAuthStore = create<AuthState>((set) => ({
+  isAuthenticated: false,
+  user: null,
+  isLoading: true,
+
+    setIsAuthenticated: (value) => set({isAuthenticated: value}),
+    setUser: (user) => set({user}),
+    setLoading: (value) => set({isLoading: value}),
+
+    fetchAuthenticatedUser : async () => {
+        set({isLoading: true});
+        
+        try { 
+            const user = await getCurrentUser();
+
+            if(user) set({isAuthenticated: true, user: user as User})
+                else set({isAuthenticated: false, user: null})
+        } catch (error) {
+            console.log('fetchAuthenticatedUser error', error)
+            set({isAuthenticated: false, user: null})
+            
+        } finally {
+            set({isLoading: false})
+        }
+    }
 }))
